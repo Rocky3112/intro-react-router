7a6112616f8eddba2c9daf01a6bd238071d4a24b
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-04-06 02:52:53 +0600

    show data & back previous using button

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './post.css'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./post.css";
 
-const Post = ({post}) => {
-    const {id, title,body}=post;
-    return (
-        <div className='post'>
-           <h2>ID: {id}</h2> 
-           <h4>Title: {title}</h4>
-           <p>{body}</p>
-           <Link to ={`/post/${id}`}>Show details </Link>
-           <Link to ={`/post/${id}`}><button>Show post details</button></Link>
-           
-        </div>
-    );
+const Post = ({ post }) => {
+  const { id, title, body } = post;
+
+  const navigate = useNavigate();
+  const handleNavigation = () => {
+    navigate(`/post/${id}`);
+  };
+  return (
+    <div className="post">
+      <h2>ID: {id}</h2>
+      <h4>Title: {title}</h4>
+      <p>{body}</p>
+      <Link to={`/post/${id}`}>Show details </Link>
+      <Link to={`/post/${id}`}>
+        <button>Show post details</button>
+      </Link>
+
+      <button onClick={handleNavigation}>button Handler</button>
+    </div>
+  );
 };
 
 export default Post;
--- src/components/PostDetail/PostDetail.jsx
+++ src/components/PostDetail/PostDetail.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetail = () => {
     const data = useLoaderData();
-    console.log(data)
+    // console.log(data)
     const {id, title,body}=data;
+
+    const navigate = useNavigate();
+    const handleBack =()=>{
+        navigate(-1);
+    }
     return (
         <div>
             <h1>this is post detail of {id}</h1>
             <h3>{title}</h3>
             <p>{body}</p>
+            <button onClick={handleBack}>Go back</button>
         </div>
     );
 };
